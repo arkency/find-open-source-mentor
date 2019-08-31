d45b42dbd43f1fd834b66f3f229617242566a470
Hubert <47819015+hkopij@users.noreply.github.com>
2019-08-31 19:48:21 +0200

    Update 20190831163306_create_developers.rb
    Delete unnecessary boolean

--- db/migrate/20190831163306_create_developers.rb
+++ db/migrate/20190831163306_create_developers.rb
@@ -2,7 +2,6 @@ class CreateDevelopers < ActiveRecord::Migration[5.2]
   def change
     create_table :developers do |t|
       t.string :name
-      t.boolean :junior, default: false
       t.timestamps
     end
   end
