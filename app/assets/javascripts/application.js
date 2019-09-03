2dda2d7831ff443f5e1f6c7de326e97f40762f33
pyrin andrii <pyrin.andrii@gmail.com>
2019-09-03 20:38:42 +0300

    Add bootstrap and simple navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+//= require jquery3
+//= require popper
+//= require bootstrap
