c13bbc4514afe9a2710df29c2c10d0ae6f689b39
Oxana Oxana <oksana.oleshchenko@gmail.com>
2019-09-04 09:02:43 +0300

    allow fetching repos from github

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,7 +2,6 @@
 
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
-    # binding.pry
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect(@user)
   end
