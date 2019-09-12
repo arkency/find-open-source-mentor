44d030990367a4446122962dff46a81b9401614c
Oxana Oxana <oksana.oleshchenko@gmail.com>
2019-09-12 19:56:06 +0300

    fix

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,6 +4,7 @@ class RepositoriesController < ApplicationController
   def new
     @client      = Octokit::Client.new
     @github_user = @client.user(current_user.uid)
+    binding.pry
     @repos       = @client.repos(@github_user.login, query: { type: 'owner', sort: 'asc' })
   end 
 end
