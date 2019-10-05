ddac6e60e8aa6288ce126ffa078f533f8d9f5a47
Stetsyshyn <volodymyr.s@done-it.net>
2019-10-05 23:27:58 +0300

    get query param role

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,7 +2,7 @@
 
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env['omniauth.auth'], request.env['omniauth.params'])
     sign_in_and_redirect(@user)
   end
 end
