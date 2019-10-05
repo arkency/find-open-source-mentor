2e40c2f53a2455298d3bf5e8e60f4c1bd9175c0c
Stetsyshyn <volodymyr.s@done-it.net>
2019-10-05 23:31:36 +0300

    add migration user roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_165222) do
+ActiveRecord::Schema.define(version: 2019_10_05_194627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_09_05_165222) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
