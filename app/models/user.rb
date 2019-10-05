5fe3783c12545fabf89c5f0357a6aebfbce8b2c3
Stetsyshyn <volodymyr.s@done-it.net>
2019-10-05 23:29:11 +0300

    add role to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  ROLES = %w[mentee mentor].freeze
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
+  has_many :projects
+  validates :role, presence: true, inclusion: { in: ROLES }
 
-  def self.from_omniauth(auth)
+  enum role: ROLES
+
+  def self.from_omniauth(auth, params)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid      = auth.info.nickname
+      user.role     = params['role']
       user.email    = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
   end
-
-  has_many :projects
 end
